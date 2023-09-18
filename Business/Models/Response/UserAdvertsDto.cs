8142eafe4268f5a4c5f37e5e68973f4ec3d6e0e2
FURKAN AYDIN <frknydn22@icloud.com>
2023-09-18 15:00:54 +0300

    Response Dtolar tanımlandı

---
+++ Business/Models/Response/UserAdvertsDto.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Data.Postgres.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models.Response
+{
+    public class UserAdvertsDto
+    {
+        public int Id { get; set; }
+        public IList<Advert> Adverts { get; set; }
+    }
+}
