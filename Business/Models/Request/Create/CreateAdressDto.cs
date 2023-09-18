0ede638641b88ca6a71a69760f00ba57c42fde27
Recep ÖZKARA <97729329+Receep24@users.noreply.github.com>
2023-09-18 14:59:24 +0300

    Create ve Update Dto girildi

---
+++ Business/Models/Request/Create/CreateAdressDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models.Request.Create
+{
+    public class CreateAdressDto
+    {
+        public string AdressName { get; set; }
+
+        public int CityId { get; set; }
+        public int DistrictId { get; set; }
+    }
+}
---
+++ Business/Models/Request/Update/UpdateCommentDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models.Request.Update
+{
+    public class UpdateCommentDto
+    {
+        public string CommentText { get; set; }
+
+        public int UserID { get; set; }
+      
+    }
+}
